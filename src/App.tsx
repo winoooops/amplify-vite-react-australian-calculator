1e169aff871ad30373336dbc4f383b3cd5d68759
winoooops <winoooops@outlook.com>
2025-09-14 14:51:13 +0800

    comp: add tanstack router with .rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,25 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [taxConfigs, setTaxConfigs] = useState<Array<Schema["TaxConfig"]["type"]>>([]);
+  const { signOut } = useAuthenticator();
 
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
+    client.models.TaxConfig.observeQuery().subscribe({
+      next: (data) => setTaxConfigs([...data.items]),
     });
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+  console.log(taxConfigs);
 
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
+      <button onClick={signOut}>Sign Out</button>
     </main>
   );
 }
